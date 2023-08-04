0f45a85a4a5a6318470fe78c9fa2aef83e675466
Jaedon <JAEDONLAU68.22@ichat.sp.edu.sg>
2023-08-04 10:51:03 +0800

    New temperaturehumidity

---
+++ Data sending.py
@@ -0,0 +1,9 @@
+import time
+import requests
+
+temperature = [1, 2, 3, 5, 6, 7, 8]
+humidity = [7, 3, 7, 5, 4, 2, 1]
+
+for x in range(7):
+    resp=requests.get('https://api.thingspeak.com/update?api_key=JKSMXUYL0QC1K64I&field1=%s&field2=%s' %(temperature[x], humidity[x]))
+    time.sleep(10)
--- temhum.py
+++ temhum.py
@@ -1,14 +1,15 @@
 import Adafruit_DHT
+import requests
+import time
 
 
 sensor = Adafruit_DHT.AM2302
 pin = 5
 
-while(1):
-    humidity,temperature = Adafruit_DHT.read_retry(sensor,pin)
-
+while (1):
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
-        print("temp = {0:.0.1f}*C Humidity={0.0:1f}%" .format(temperature,humidity))
-
+        resp = req.get('https://api.thingspeak.com/update?api_key=R6J56QOW5UG7ALMV&field1=%s&field2=%s' % (temperature, humidity))
     else:
-        print("non")
+        print("Failed to get reading, will try again")
+    sleep(2)
